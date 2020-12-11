aa50574c684ab150893512c6b29c2aeaca1dd870
emily <emily.lawlor@gmail.com>
2020-12-11 19:14:38 +0000

    Test added to check if the componts values are valid

--- src/test/java/Colour/ColourTest.java
+++ src/test/java/Colour/ColourTest.java
@@ -1,6 +1,5 @@
 package Colour;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,6 +10,18 @@ class ColourTest {
     @Test
     public void acceptThreeComponentsOnly(){
         colour = new Colour(10, 20, 30);
+    }
+
+    @Test
+    public void acceptValidComponentValues(){
+        colour = new Colour(10,20, 30);
+        assertNotNull(colour);
+    }
+
+    @Test
+    public void rejectInvalidComponentValues(){
+        colour = new Colour(100000, -10, 3);
+        assertNull(colour);
     }
 
 }
