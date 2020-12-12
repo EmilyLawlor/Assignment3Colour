75df64a11deddb053bab8517fde213cc031848d1
emily <emily.lawlor@gmail.com>
2020-12-12 22:19:49 +0000

    test which ensure exception is thrown if invalid values are used for colour object creation

--- src/main/java/Colour/Colour.java
+++ src/main/java/Colour/Colour.java
@@ -11,5 +11,4 @@ public class Colour {
         this.b = b;
     }
 
-
 }
--- src/test/java/Colour/ColourTest.java
+++ src/test/java/Colour/ColourTest.java
@@ -10,18 +10,21 @@ class ColourTest {
     @Test
     public void acceptThreeComponentsOnly(){
         colour = new Colour(10, 20, 30);
-    }
-
-    @Test
-    public void acceptValidComponentValues(){
-        colour = new Colour(10,20, 30);
         assertNotNull(colour);
     }
 
     @Test
-    public void rejectInvalidComponentValues(){
-        colour = new Colour(100000, -10, 3);
-        assertNull(colour);
+    public void acceptValidComponentValues() {
+        colour = new Colour(10, 20, 30);
+        assertNotNull(colour);
     }
+
+    @Test
+    public void rejectInvalidComponentValues() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            colour = new Colour(10000, -24, 12345);
+        });
+    }
+
 
 }
