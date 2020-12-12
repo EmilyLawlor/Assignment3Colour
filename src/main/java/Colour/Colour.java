b939f6cc2be83038cdec9bab965c4f332c74c172
emily <emily.lawlor@gmail.com>
2020-12-12 22:40:01 +0000

    Colour class updated to only except valid component values

--- src/main/java/Colour/Colour.java
+++ src/main/java/Colour/Colour.java
@@ -5,10 +5,14 @@ public class Colour {
     private float g;
     private float b;
 
-    public Colour(float r, float g, float b){
-        this.r = r;
-        this.g = g;
-        this.b = b;
+    public Colour(float r, float g, float b) {
+        if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255){
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        } else {
+           throw new IllegalArgumentException("Values must be between 0 and 255");
+        }
     }
 
 }
